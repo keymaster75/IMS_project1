d45be0ad59ad5a4fbb4a2ef0b91345c75f01d0f1
Aleksandar Aleksic <42803237+keymaster75@users.noreply.github.com>
2023-03-11 12:37:58 +0100

    Promena IP adrese i ostalo

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,61 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Demo\DemoController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Home\HomeSlideController;
+use App\Http\Controllers\Home\AboutController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
 
-Route::controller(DemoController::class)->group(function(){
-    Route::get('/about', 'Index')->name('about.page');
+
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about', 'Index')->name('about.page')->middleware('check');
     Route::get('/contact', 'ContactMethod')->name('contact.page');
 });
 
 
+// Admin All Route
+Route::controller(AdminController::class)->group(function () {
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+    Route::get('/admin/profile', 'Profile')->name('admin.profile');
+    Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
+    Route::post('/store/profile', 'StoreProfile')->name('store.profile');
+
+    Route::get('/change/password', 'ChangePassword')->name('change.password');
+    Route::post('/update/password', 'UpdatePassword')->name('update.password');
+
+});
+
+// Home Slide All Route
+Route::controller(HomeSlideController::class)->group(function () {
+    Route::get('/home/slide', 'HomeSlider')->name('home.slide');
+    Route::post('/update/slide', 'UpdateSlider')->name('update.slider');
+
+});
+
+// About All Route
+Route::controller(AboutController::class)->group(function () {
+    Route::get('/about/page', 'AboutPage')->name('about.page');
+    Route::post('/update/about', 'UpdateAbout')->name('update.about');
+    Route::get('/about', 'HomeAbout')->name('home.about');
+    Route::get('/about/multi/image', 'AboutMultiImage')->name('about.multi.image');
+    Route::post('/store/multi/image', 'StoreMultiImage')->name('store.multi.image');
+});
+
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
